89cd6ff560065feb08a32d830f8ed78240992699
Robrecht Dewaele <robrecht@xaop.com>
2014-12-01 15:59:43 +0100

    also init the submodules in the script to save some effort

--- submodules-init.sh
+++ submodules-init.sh
@@ -4,4 +4,6 @@
 # http://stackoverflow.com/questions/1777854/git-submodules-specify-a-branch-tag
 # (answer by VonC)
 
+git submodule update --init
+
 git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
