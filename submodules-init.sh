b103f72591e99aed1bfd792a4579ebb5e82863ec
Robrecht Dewaele <robrecht@xaop.com>
2014-12-01 17:41:54 +0100

    git config updated

--- submodules-init.sh
+++ submodules-init.sh
@@ -7,3 +7,6 @@
 git submodule update --init
 
 git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
+
+# ignore local changes as these files will contain sensitive information
+git update-index --assume-unchanged git/gitconfig.symlink
